ccedfc0fdbec8e63bdb39ffb98ef2040e53e9f37
Ethicaldisguise <7862sadikd@gmail.com>
2024-11-07 00:20:20 +0530

    completed repo

--- backend/election/repository/poll.py
+++ backend/election/repository/poll.py
@@ -1,21 +1,62 @@
-from db.database import AsyncDB
-from repository.interface import Repository
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from election.db.schemas import ElectionSchema
+from election.repository.interface import Repository
 
 
 class PollRepository(Repository):
 
-    def __init__(self, _db: AsyncDB):
+    def __init__(self, _db):
         super().__init__(_db)
 
-    async def create(self, item):
-        async with self._database() as async_session:
-            ...
+    async def create(self, item: ElectionSchema):
+        async with self.database() as async_session:
+            async_session: AsyncSession
+            async_session.add(item)
 
-    def read(self, item_id):
-        pass
+    async def read(self, item_id) -> ElectionSchema:
+        async with self.database() as async_session:
+            async_session: AsyncSession
+            result = await async_session.execute(
+                select(ElectionSchema).filter_by(election_id=item_id)
+            )
+            return result.scalars().first()  # Get the first result
 
-    def update(self, item_id, item):
-        pass
+    async def update(self, item_id, item: ElectionSchema):
+        async with self.database() as async_session:
+            async_session: AsyncSession
+            existing_item = await self.read(item_id)
+            if existing_item is None:
+                raise NoResultFound("Election not found.")
 
-    def delete(self, item_id):
-        pass
+            existing_item.election_id = item.election_id
+            existing_item.description = item.description
+            existing_item.start_date = item.start_date
+            existing_item.end_date = item.end_date
+            existing_item.total_candidates = item.total_candidates
+            existing_item.total_voters = item.total_voters
+            existing_item.title = item.title
+            existing_item.election_status = item.election_status
+            existing_item.validation_regex = item.validation_regex
+            await async_session.merge(existing_item)
+
+    async def delete(self, item_id):
+        async with self.database() as async_session:
+            async_session: AsyncSession
+            # Fetch the existing item first
+            existing_item = await self.read(item_id)
+            if existing_item is None:
+                raise NoResultFound("Election not found.")
+
+            await async_session.delete(existing_item)  # Delete the existing item
+
+    async def increment_total_voters(self, poll_id):
+        async with self.database() as async_session:
+            async_session: AsyncSession
+            result = await async_session.execute(
+                select(ElectionSchema).filter_by(election_id=poll_id)
+            )
+            result = result.scalars().first()  # Get the first result
+            result.total_voters += 1
--- backend/election/repository/voter.py
+++ backend/election/repository/voter.py
@@ -1,26 +1,68 @@
+from sqlalchemy import and_, delete, select, update
+from sqlalchemy.exc import MultipleResultsFound
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from repository.interface import Repository
+from election.core.models.user import VoterInDb
+from election.db.schemas import VoterSchema
+from election.repository.interface import Repository
 
 
 class VoterRepository(Repository):
 
-    async def add_voter(self):
-        async with self._database() as async_session:
+    async def create(self, item: VoterInDb):
+        async with self.database() as async_session:
             async_session: AsyncSession
+            voter = VoterSchema(**item.model_dump())
+            async_session.add(voter)
+            return voter
 
-    async def create(self, item):
-        async with self._database() as async_session:
+    async def read(self, ids) -> VoterInDb | None:
+        voter_id, election_id = ids
+        async with self.database() as async_session:
             async_session: AsyncSession
+            query = select(VoterSchema).where(
+                and_(
+                    VoterSchema.user_id == voter_id,
+                    VoterSchema.election_id == election_id,
+                )
+            )
 
-    async def read(self, item_id):
-        async with self._database() as async_session:
-            async_session: AsyncSession
+            voter_entry = (await async_session.execute(query)).first().tuple()
+            if voter_entry:
+                voter_entry = voter_entry[0]
+                return VoterInDb.model_validate(voter_entry)
+            return None
 
-    async def update(self, item_id, item):
-        async with self._database() as async_session:
+    async def update(self, ids, voter_update: VoterInDb):
+        voter_id, election_id = ids
+        async with self.database() as async_session:
             async_session: AsyncSession
+            query = update(VoterSchema).where(
+                and_(
+                    VoterSchema.user_id == voter_id,
+                    VoterSchema.election_id == election_id,
+                )
+            ).values(**voter_update.model_dump())
+            await async_session.execute(query)
+            return await self.read(ids)
 
-    async def delete(self, item_id):
-        async with self._database() as async_session:
+    async def delete(self, voter_id):
+        async with self.database() as async_session:
             async_session: AsyncSession
+            query = delete(VoterSchema).where(VoterSchema.user_id == voter_id)
+            await async_session.execute(query)
+
+    async def mark_voted(self, voter_id, poll_id):
+        async with self.database() as async_session:
+            async_session: AsyncSession
+            stmt = select(VoterSchema).where(
+                and_(
+                    VoterSchema.user_id == voter_id,
+                    VoterSchema.election_id == poll_id,
+                )
+            )
+            try:
+                result = (await async_session.execute(stmt)).scalar_one_or_none()  # Fetches a single instance or None
+                result.has_voted = True
+            except MultipleResultsFound as mrf:
+                raise ReferenceError("wtf") from mrf
