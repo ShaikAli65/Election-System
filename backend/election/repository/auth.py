3c017659ba55eb6b2af809d79795a395f5481c7a
Ethicaldisguise <7862sadikd@gmail.com>
2024-11-07 00:12:50 +0530

    storing keys as generalized strings

--- backend/election/repository/auth.py
+++ backend/election/repository/auth.py
@@ -1,27 +1,27 @@
 from uuid import UUID
 
-from core.models.user import UserLoggedInCookie
-from repository.interface import Repository
+from election.core.models.user import UserLoggedInCookie
+from election.repository.interface import Repository
 
 
 class AuthRepository(Repository):
 
     async def read(self, item_id):
-        async with self._database() as database:
+        async with self.database() as database:
             database: dict[UUID, UserLoggedInCookie]
-            return database.get(item_id)
+            return database.get(str(item_id))
 
     async def update(self, item_id, item: UserLoggedInCookie):
-        async with self._database() as database:
+        async with self.database() as database:
             database: dict[UUID, UserLoggedInCookie]
-            database[item_id] = item
+            database[str(item_id)] = item
 
     async def delete(self, item_id):
-        async with self._database() as database:
+        async with self.database() as database:
             database: dict[UUID, UserLoggedInCookie]
-            return database.pop(item_id)
+            return database.pop(str(item_id))
 
     async def create(self, item: tuple[UUID, UserLoggedInCookie]):
-        async with self._database() as database:
+        async with self.database() as database:
             database: dict[UUID, UserLoggedInCookie]
-            database[item[0]] = item[1]
+            database[str(item[0])] = item[1]
