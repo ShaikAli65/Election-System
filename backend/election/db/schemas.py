44f3623d818b66b2ad71138e4b2263712c9b8b2e
Ethicaldisguise <7862sadikd@gmail.com>
2024-11-07 00:17:17 +0530

    added Schema prefix,

--- backend/election/db/schemas.py
+++ backend/election/db/schemas.py
@@ -1,29 +1,36 @@
 # schemas.py
+
 from sqlalchemy import Boolean, Column, Enum, ForeignKey, Integer, String, TIMESTAMP, Text
-from sqlalchemy.dialects.postgresql import UUID as PGUUID
+from sqlalchemy.dialects.postgresql import ENUM, UUID as PGUUID
 from sqlalchemy.sql import func
 
 from .database import Base
 
 
-class Voter(Base):
+class VoterSchema(Base):
     __tablename__ = "voters"
 
-    user_id = Column(PGUUID(as_uuid=True), ForeignKey("authentication.user_id"), primary_key=True, unique=True,
-                     nullable=False)
+    user_id = Column(PGUUID(as_uuid=True), ForeignKey("authentication.user_id"), primary_key=True,)
     has_voted = Column(Boolean, default=False)
-    election_id = Column(PGUUID(as_uuid=True), ForeignKey("elections.election_id"))
+    election_id = Column(PGUUID(as_uuid=True), ForeignKey("elections.election_id"), primary_key=True)
 
 
-class Candidate(Base):
+class CandidateSchema(Base):
+
     __tablename__ = "candidates"
 
     candidate_id = Column(PGUUID(as_uuid=True), ForeignKey("authentication.user_id"), primary_key=True)
-    election_id = Column(PGUUID(as_uuid=True), ForeignKey("elections.election_id"), nullable=False)
-    candidate_description = Column(Text)
+    word_from_candidate = Column(Text)
     manifesto_file_path = Column(String(255))
     total_votes = Column(Integer, default=0)
     nomination_date = Column(TIMESTAMP, server_default=func.now())
+    email_id = Column(Text)
+    candidate_name = Column(String(255))
+    election_id = Column(PGUUID(as_uuid=True), ForeignKey("elections.election_id"), nullable=False)
+
+    def __repr__(self):
+        return f"<CandidateSchema(cid={self.candidate_id}, cname={self.candidate_name}, eid={self.election_id})>"
+    # election = relationship('Election', back_populates='candidates')
 
 
 class AccountStatusEnum(str, Enum):
@@ -32,7 +39,7 @@ class AccountStatusEnum(str, Enum):
     INACTIVE = "inactive"
 
 
-class Authentication(Base):
+class AuthenticationSchema(Base):
     __tablename__ = "authentication"
 
     user_id = Column(PGUUID(as_uuid=True), primary_key=True)  # UUID type for primary key
@@ -46,29 +53,33 @@ class ElectionStatusEnum(str, Enum):
     COMPLETED = "completed"
 
 
-class Ballot(Base):
+class BallotSchema(Base):
     __tablename__ = "ballots"
 
-    ballot_id = Column(PGUUID(as_uuid=True), primary_key=True)
-    voter_id = Column(PGUUID(as_uuid=True), ForeignKey("voters.user_id"), nullable=False)
-    election_id = Column(PGUUID(as_uuid=True), ForeignKey("elections.election_id"), nullable=False)
+    voter_id = Column(PGUUID(as_uuid=True), ForeignKey("voters.user_id"), nullable=False, primary_key=True)
+    election_id = Column(PGUUID(as_uuid=True), ForeignKey("elections.election_id"), nullable=False, primary_key=True)
     candidate_id = Column(PGUUID(as_uuid=True), ForeignKey("candidates.candidate_id"), nullable=False)
     voting_time = Column(TIMESTAMP, server_default=func.now())
 
+    def __str__(self):
+        return f"<BallotSchema(eid={self.election_id}, cid={self.candidate_id}, vid={self.voter_id}, t={self.voting_time})>"
 
-class Election(Base):
+
+class ElectionSchema(Base):
     __tablename__ = "elections"
-
     election_id = Column(PGUUID(as_uuid=True), primary_key=True)
-    election_name = Column(String(255), nullable=False)
-    election_start_date = Column(TIMESTAMP, nullable=False)
-    election_end_date = Column(TIMESTAMP, nullable=False)
+    title = Column(String(255), nullable=False)
+    start_date = Column(TIMESTAMP, nullable=False)
+    end_date = Column(TIMESTAMP, nullable=False)
     total_voters = Column(Integer, default=0)
     total_candidates = Column(Integer, default=0)
-    election_status = Column(Enum(ElectionStatusEnum()), default="upcoming", nullable=False)
+    election_status = Column(ENUM(ElectionStatusEnum.UPCOMING, ElectionStatusEnum.ONGOING, ElectionStatusEnum.COMPLETED, name='election_status'), default=ElectionStatusEnum.UPCOMING, nullable=False)
+    description = Column(String(1024), nullable=False)
+    validation_regex = Column(String(255))
 
 
-class AuditLog(Base):
+
+class AuditLogSchema(Base):
     __tablename__ = "audit_log"
 
     log_id = Column(PGUUID(as_uuid=True), primary_key=True)
@@ -78,17 +89,10 @@ class AuditLog(Base):
     details = Column(Text)  # Optional field for additional details
 
 
-class resultStatusEnum(str, Enum):
-    winner = "winner"
-    loser = "loser"
-    tie = "tie"
-
-
-class ElectionResult(Base):
+class ElectionResultSchema(Base):
     __tablename__ = "election_results"
 
     result_id = Column(PGUUID(as_uuid=True), primary_key=True)
     election_id = Column(PGUUID(as_uuid=True), ForeignKey("elections.election_id"), nullable=False)
     candidate_id = Column(PGUUID(as_uuid=True), ForeignKey("candidates.candidate_id"), nullable=False)
     total_votes = Column(Integer, default=0)
-    result_status = Column(Enum(resultStatusEnum()))
