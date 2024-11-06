ad6520d5bab1fbf9bf771b5ec5b9d402007f9b02
Ethicaldisguise <7862sadikd@gmail.com>
2024-11-07 00:23:27 +0530

    guarded poll_id to be a string

--- backend/election/utils/files.py
+++ backend/election/utils/files.py
@@ -1,11 +1,11 @@
 import shutil
 
-from backend.election.core.constants import get_config
+from election.core.constants import get_config
 
 
 def get_porfolio_path(poll_id, candidate_id):
     base_path = get_config().portfolios_path
-    poll_path = base_path / poll_id
+    poll_path = base_path / str(poll_id)
     portfolio_path = poll_path / f"{candidate_id}.pdf"
     portfolio_path.resolve()
     if portfolio_path.is_relative_to(base_path) and portfolio_path.exists():
@@ -16,16 +16,16 @@ def get_porfolio_path(poll_id, candidate_id):
 
 def generate_portfolio_path(poll_id, candidate_id):
     base_path = get_config().portfolios_path
-    poll_path = base_path / poll_id
+    poll_path = base_path / str(poll_id)
     portfolio_path = poll_path / f"{candidate_id}.pdf"
     portfolio_path.resolve()
     poll_path.mkdir(parents=True, exist_ok=True)
-    print("generated path", type(portfolio_path), portfolio_path, "for", poll_id, candidate_id)
+    # print("generated path", type(portfolio_path), portfolio_path, "for", poll_id, candidate_id)
     return portfolio_path
 
 
 async def save_porfolio(file_path, file_object):
     # file_path
     with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file_object.file, buffer)
+        shutil.copyfileobj(file_object, buffer)
 
