437e27ee91dce9c6fa90ef26746777f324f33fab
Ethicaldisguise <7862sadikd@gmail.com>
2024-11-07 00:17:43 +0530

    wrote a driver function for tests

--- backend/tests/temp.py
+++ backend/tests/temp.py
@@ -1,39 +1,15 @@
-# import datetime
-# from pathlib import Path
-#
-# from fastapi import FastAPI
-#
-# from backend.core.models.poll import CandidateInPoll, PollId, PortFolio, Poll
-# from backend.core.models.person import PersonId
-#
-# p = Path("C:\\Users\\7862s\\Desktop\\Election-System\\database\\portfolios\\notavailable.pdf")
-#
-# poll_id = PollId(1)
-#
-# poll = Poll(
-#     title="test",
-#     type="normal",
-#     start_date=datetime.date(2024,12,4),
-#     end_date=datetime.date(2024,12,4),
-#     candidates=[
-#         CandidateInPoll(
-#             candidate_id=PersonId("1"),
-#             # portfolio=PortFolio(
-#             #     portfolio_path=p,
-#             #     poll_id=poll_id,
-#             #     candidate_id=PersonId("1"),
-#             # ),
-#         ),
-#         CandidateInPoll(
-#             candidate_id=PersonId("1"),
-#             portfolio=PortFolio(
-#                 portfolio_path=p,
-#                 poll_id=poll_id,
-#                 candidate_id=PersonId("1"),
-#             ),
-#         ),
-#     ]
-# )
-#
-# app = FastAPI()
-#
+import asyncio
+
+import election.db.schemas  # noqa
+from election.main import life_span
+from units.test_results import test_results
+
+
+async def main():
+    async with life_span(None) as _:
+        # await test_poll_views()
+        await test_results('4ac072ed-9778-4569-8a80-5f5eeddb8cde')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
